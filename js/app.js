fcd9db0ed16f616518edfb1d5da385fb3a9b09df
dvcode <daviolivero2103@gmail.com>
2022-12-22 11:24:39 -0400

    proyecto finalizado

---
+++ js/app.js
@@ -0,0 +1,24 @@
+// Elements
+const adviceNumber = document.querySelector('.advice__number');
+const adviceContent = document.querySelector('.advice__content');
+const adviceBtn = document.querySelector('.advice__btn');
+
+eventListeners();
+function eventListeners(){
+    adviceBtn.addEventListener('click', showAdvice);
+    document.addEventListener('DOMContentLoaded', showAdvice);
+}
+
+async function showAdvice(){
+    try {
+        const url = 'https://api.adviceslip.com/advice';
+    
+        const response = await fetch(url);
+        const result = await response.json();
+
+        adviceNumber.innerText = result.slip.id;
+        adviceContent.innerText = `"${result.slip.advice}"`;
+    } catch (error) {
+        console.log(error);
+    }
+}
